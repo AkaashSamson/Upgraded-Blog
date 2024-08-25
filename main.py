cc5cb624d80afaabda60ea32276e321c5350d0ce
AkaashSamson <akaashsam22@gmail.com>
2024-08-25 09:22:41 +0530

    use api to get posts

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import requests
+
+response = requests.get('https://api.npoint.io/611d72b4a5dc1bf578bd')
+data = response.json()
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', data=data)
 
 @app.route('/about')
 def about():
@@ -13,6 +17,12 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/post/<post_id>')
+def post(post_id):
+    id = int(post_id) - 1
+    post = data[id]
+    return render_template('post.html', post=post)
 
 if __name__ == '__main__':
     app.run(debug=True)
