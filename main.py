f541bafac79252a7a10b1613006ab7a25e098f70
AkaashSamson <akaashsam22@gmail.com>
2024-09-05 04:51:28 +0530

    Contact form starts working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 response = requests.get('https://api.npoint.io/611d72b4a5dc1bf578bd')
@@ -14,15 +14,33 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return render_template('contact.html', msg_sent=True)
+    else:
+        return render_template('contact.html', msg_sent=False)
 
 @app.route('/post/<post_id>')
 def post(post_id):
     id = int(post_id) - 1
     post = data[id]
     return render_template('post.html', post=post)
+
+# @app.route('/form-submit', methods=['POST'])
+# def form_submit():
+#     data = request.form
+#     print(data["name"])
+#     print(data["email"])
+#     print(data["phone"])
+#     print(data["message"])
+#     return "<h1>Successfully sent your message</h1>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
