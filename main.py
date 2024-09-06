c08e87d59c1ebfa25947caf3b4426abf385afbc0
AkaashSamson <akaashsam22@gmail.com>
2024-09-06 07:42:03 +0530

    activate sending emails

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+
+my_email = "akaashsam22@gmail.com"
+my_passkey = "jtljflkpcjcsizwb"
+
+connection = smtplib.SMTP("smtp.gmail.com") # this line is used to connect to the smtp server
+connection.starttls() # this line is used to secure the connection which means that the data is encrypted
+connection.login(user=my_email, password=my_passkey) # this line is used to login to the email account
+
 
 response = requests.get('https://api.npoint.io/611d72b4a5dc1bf578bd')
 data = response.json()
@@ -18,10 +27,11 @@ def about():
 def contact():
     if request.method == 'POST':
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        name = data["name"]
+        email = data["email"]
+        phone = data["phone"]
+        message = data["message"]
+        connection.sendmail(from_addr=my_email, to_addrs=my_email, msg=f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}")
         return render_template('contact.html', msg_sent=True)
     else:
         return render_template('contact.html', msg_sent=False)
